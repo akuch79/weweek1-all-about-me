060e5b407d6919d91799e5d4515ccad574ab15c3
Margaret Akuch Deng <margaretakuch2019@gmail.com>
2026-08-18 17:17:19 +0300

    Add personal information and introduction in Python

---
+++ all_about_me.py
@@ -0,0 +1,11 @@
+name = "Margaret"
+age = 25
+country = "South Sudan"
+hobbies = "learning new skills and helping others"
+interesting_fact = "I enjoy learning technology and using it to solve problems."
+
+print("My name is", name)
+print("I am", age, "years old")
+print("I am from", country)
+print("My hobbies are", hobbies)
+print("Interesting fact about me:", interesting_fact)
